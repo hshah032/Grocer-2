7a5c6939919ae0eadde44113b6ed7e42811bc228
Hussam Shah <51976566+hshah032@users.noreply.github.com>
2020-06-09 20:54:22 -0600

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,178 @@
+
+// This function is called when any of the tab is clicked
+// It is adapted from https://www.w3schools.com/howto/howto_js_tabs.asp
+
+function openInfo(evt, tabName) {
+
+	// Get all elements with class="tabcontent" and hide them
+	tabcontent = document.getElementsByClassName("tabcontent");
+	for (i = 0; i < tabcontent.length; i++) {
+		tabcontent[i].style.display = "none";
+	}
+
+	// Get all elements with class="tablinks" and remove the class "active"
+	tablinks = document.getElementsByClassName("tablinks");
+	for (i = 0; i < tablinks.length; i++) {
+		tablinks[i].className = tablinks[i].className.replace(" active", "");
+	}
+
+	// Show the current tab, and add an "active" class to the button that opened the tab
+	document.getElementById(tabName).style.display = "block";
+	evt.currentTarget.className += " active";
+
+}
+
+
+	
+// generate a checkbox list from a list of products
+// it makes each product name as the label for the checkbos
+
+function populateListProductChoices(slct1) {
+	if(document.getElementById(slct1).checked == true) {
+	var s1 = document.getElementById(slct1);
+
+    var s2;
+			
+	// obtain a reduced list of products based on restrictions
+	var optionArrayRand = restrictListProducts(products, s1.value);
+	var optionArray = sortByPrice(optionArrayRand);
+
+	var produce = restrictMore(optionArray, "produce");
+	var meats = restrictMore(optionArray, "meats");
+	var breads = restrictMore(optionArray, "breads");
+	var cookies = restrictMore(optionArray, "cookies");
+
+	
+	var prod = true;
+	var meat = true;
+	var bread = true;
+	var cook = true;
+
+	if(prod){
+
+		s2 = document.getElementById("displayProduce");
+	
+		s2.innerHTML = "";
+	
+		renderElements(s2, produce);
+
+		prod = false;
+	}
+	
+	if(meat){
+
+		s2 = document.getElementById("displayMeat");
+	
+		s2.innerHTML = "";
+	
+		renderElements(s2, meats);
+
+		meat = false;
+	}
+
+	if(bread){
+
+		s2 = document.getElementById("displayBread");
+	
+		s2.innerHTML = "";
+	
+		renderElements(s2, breads);
+
+		bread = false;
+
+	}
+
+	if(cook){
+		
+		s2 = document.getElementById("displayCook");
+	
+		s2.innerHTML = "";
+	
+		renderElements(s2, cookies);
+
+		cook = false;
+	}
+
+}
+	
+}
+
+function renderElements(s2, optionArray){
+	
+	for (i = 0; i < optionArray.length; i++) {
+
+		var productName = optionArray[i];
+
+		// create the checkbox and add in HTML DOM
+		var checkbox = document.createElement("input");
+		checkbox.type = "checkbox";
+		checkbox.name = "product";
+		checkbox.value = productName.name;
+		s2.appendChild(checkbox);
+
+		// create a label for the checkbox, and also add in HTML DOM
+		var label = document.createElement('label');
+		label.htmlFor = productName;
+		label.appendChild(document.createTextNode(productName.name));
+		label.innerHTML += ' ';
+		label.appendChild(document.createTextNode(productName.price));
+		s2.appendChild(label);
+
+		var image = document.createElement("img");
+		image.src = productName.src;
+		s2.appendChild(image);
+
+		// create a breakline node and add in HTML DOM
+		s2.appendChild(document.createElement("br")); 
+	}   
+}
+	
+// This function is called when the "Add selected items to cart" button in clicked
+// The purpose is to build the HTML to be displayed (a Paragraph) 
+// We build a paragraph to contain the list of selected items, and the total price
+
+function selectedItems(){
+	
+	var ele = document.getElementsByName("product");
+	var chosenProducts = [];
+	
+	var c = document.getElementById('displayCart');
+	c.innerHTML = "";
+	
+	// build list of selected item
+	var para = document.createElement("P");
+	para.innerHTML = "You selected : ";
+	para.appendChild(document.createElement("br"));
+	for (i = 0; i < ele.length; i++) { 
+		if (ele[i].checked) {
+			para.appendChild(document.createTextNode(ele[i].value));
+			para.appendChild(document.createElement("br"));
+			chosenProducts.push(ele[i].value);
+		}
+	}
+		
+	// add paragraph and total price
+	c.appendChild(para);
+	c.appendChild(document.createTextNode("Total Price is " + getTotalPrice(chosenProducts)));
+		
+}
+
+// function optionSelect() {
+// 	// Get the checkbox
+// 	var checkBox = document.getElementById("myCheck");
+// 	// Get the output text
+// 	var text = document.getElementById("text");
+	
+// 	// if(text == 'Vegetarian'){
+// 	// 	text.style.display = "block";
+// 	// }
+  
+// 	// If the checkbox is checked, display the output text
+// 	if (checkBox.checked == true){
+// 	  text.style.display = "block";
+// 	} else {
+// 	  text.style.display = "none";
+// 	}
+//   }
+  
+
